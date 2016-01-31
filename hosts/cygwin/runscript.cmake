417970032eeae287251b7271f721187ab2c3a7e7
okuoku <mjt@cltn.org>
2016-01-31 20:31:09 +0900

    cygwin: Add top-level CMake project to build yunibase image

---
+++ hosts/cygwin/runscript.cmake
@@ -0,0 +1,22 @@
+# First, translate filepath into cygpath
+
+set(_pth ${CMAKE_CURRENT_LIST_DIR}/../../scripts/build-on-root.cmake)
+
+execute_process(
+    COMMAND cygpath ${_pth}
+    WORKING_DIRECTORY cyg64/bin
+    OUTPUT_VARIABLE out
+    RESULT_VARIABLE rr)
+
+if(rr)
+    message(FATAL_ERROR "Cannot translate script path ${rr}")
+endif()
+
+message(STATUS "Script path = ${out}")
+
+# Launch builder
+execute_process(
+    COMMAND cyg64/bin/bash -l -c
+    "cmake -DEXCEPT=CHIBI_SCHEME\;CHICKEN\;GAUCHE\;GUILE\;RACKET\;SAGITTARIUS\;VICARE -P ${out}"
+    )
+

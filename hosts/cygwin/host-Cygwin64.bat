13a51660182e8c797d8df0610162b2b3857f65cd
okuoku <mjt@cltn.org>
2016-02-02 02:59:22 +0900

    windows: Merge Cygwin and MSYS install scripts

--- hosts/cygwin/host-Cygwin64.bat
+++
@@ -1,5 +0,0 @@
-REM Download Cygwin installer
-REM Buildsystem: libisl10, cloog-isl (tentative)
-REM Guile: libiconv-devel
-powershell wget http://cygwin.com/setup-x86_64.exe -OutFile setup-x86_64.exe
-setup-x86_64.exe -s http://ftp.iij.ad.jp/pub/cygwin -R %~dp0\cyg64 -l %~dp0\cache64 -B -v -q -N -d -g -P libiconv-devel,libisl10,cloog-isl,cmake,git,gcc,make,gcc-g++,automake,autoconf,libtool,libgmp-devel,libonig-devel,flex,gettext-devel,pkg-config,libunistring-devel,libffi-devel,libgc-devel,texinfo,zlib-devel

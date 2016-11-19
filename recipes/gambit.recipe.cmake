8f70a5f740da5e522872f07f5e460db54a4b0c50
okuoku <mjt@cltn.org>
2016-11-20 05:15:00 +0900

    Gambit: Disable parallel build on bootstrap

--- recipes/gambit.recipe.cmake
+++ recipes/gambit.recipe.cmake
@@ -11,7 +11,7 @@ set(RECIPE_GAMBIT_BOOTSTRAP
     STEP "SetupGscBoot" cp ${YUNIBASE_BUILD_STABLE_PREFIX}/gambit/bin/gsc
     gsc-boot
     STEP "ConfigureFirst" "./configure" "--enable-single-host"
-    STEP "Bootstrap1" MAKE __MAKE_PARALLEL__ bootclean
-    STEP "Bootstrap2" MAKE __MAKE_PARALLEL__ bootstrap
-    STEP "Bootstrap3" MAKE __MAKE_PARALLEL__ bootclean
+    STEP "Bootstrap1" MAKE bootclean
+    STEP "Bootstrap2" MAKE bootstrap
+    STEP "Bootstrap3" MAKE bootclean
     ${RECIPE_GAMBIT})

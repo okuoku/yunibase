72827530ef075212913946efb00d49d4c74cf22e
okuoku <mjt@cltn.org>
2016-12-18 06:16:37 +0900

    Racket: Add --copy option to raco-pkg install

--- recipes/racket.recipe.cmake
+++ recipes/racket.recipe.cmake
@@ -12,53 +12,52 @@ set(RECIPE_RACKET # We do not support in-tree tests yet
     )
 
 set(RECIPE_RACKET_SETUP
-    STEP "SetupRacketLib" raco pkg install --skip-installed 
+    STEP "SetupRacketLib" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket/pkgs/racket-lib
 
-    STEP "SetupBase" raco pkg install --skip-installed 
+    STEP "SetupBase" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket/pkgs/base
 
-    STEP "SetupSRFILite" raco pkg install --skip-installed
+    STEP "SetupSRFILite" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/srfi/srfi-lite-lib
 
-    STEP "SetupNetLib" raco pkg install --skip-installed
+    STEP "SetupNetLib" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket/pkgs/net-lib
 
-    STEP "SetupSchemeLib" raco pkg install --skip-installed 
+    STEP "SetupSchemeLib" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/scheme-lib
 
-    STEP "SetupSourceSyntax" raco pkg install --skip-installed
+    STEP "SetupSourceSyntax" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/typed-racket/source-syntax
     
-    STEP "SetupErrortraceLib" raco pkg install --skip-installed
+    STEP "SetupErrortraceLib" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/errortrace/errortrace-lib
     
-    STEP "SetupSandboxLib" raco pkg install --skip-installed
+    STEP "SetupSandboxLib" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/sandbox-lib
 
-    STEP "SetupCompatibilityLib" raco pkg install --skip-installed
+    STEP "SetupCompatibilityLib" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/compatibility/compatibility-lib
 
-    STEP "SetupR5RS" raco pkg install --skip-installed
+    STEP "SetupR5RS" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/r5rs/r5rs-lib
 
-    STEP "SetupR6RS" raco pkg install --skip-installed
+    STEP "SetupR6RS" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/r6rs/r6rs-lib
 
-    STEP "SetupSRFI" raco pkg install --skip-installed
+    STEP "SetupSRFI" raco pkg install --skip-installed --copy
     --scope installation --batch -j __MAKE_JOBS__
     ${YUNIBASE_ROOT_CURRENT}/racket-pkgs/srfi/srfi-lib
-
     )
 

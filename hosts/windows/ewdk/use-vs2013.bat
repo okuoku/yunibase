c9759ffcc0776e4266dd84a568674aea7659dfba
okuoku <mjt@cltn.org>
2017-02-18 23:39:57 +0900

    ewdk: Add use-vs2013.bat to use VS2013 tools

---
+++ hosts/windows/ewdk/use-vs2013.bat
@@ -0,0 +1,6 @@
+set VCInstallDir_120=%BuildLabSetupFilesRoot%\Microsoft Visual Studio 12.0\VC\
+set VCTargetsPath=%BuildLabSetupFilesRoot%\MSBuild\Microsoft.Cpp\v4.0\V120\
+set VCTargetsPath12=%BuildLabSetupFilesRoot%\MSBuild\Microsoft.Cpp\v4.0\V120\
+set WindowsSDK_LibraryPath_x64=%BuildLabSetupFilesRoot%\Windows Kits\10\Lib\10.0.14393.0\um\x64
+set WindowsSDK_LibraryPath_x86=%BuildLabSetupFilesRoot%\Windows Kits\10\Lib\10.0.14393.0\um\x86
+set IncludePath=%BuildLabSetupFilesRoot%\Windows Kits\10\Include\10.0.14393.0\um;%BuildLabSetupFilesRoot%\Windows Kits\10\Include\10.0.14393.0\shared;%BuildLabSetupFilesRoot%\Microsoft Visual Studio 12.0\vc\include

de16dc9968746ba5e0306f427d1a369f4256e0c0
okuoku <mjt@cltn.org>
2017-02-22 06:35:03 +0900

    ewdk: Update IncludePath to SDK path

--- hosts/windows/ewdk/use-vs2013.bat
+++ hosts/windows/ewdk/use-vs2013.bat
@@ -3,4 +3,4 @@ set VCTargetsPath=%BuildLabSetupFilesRoot%\MSBuild\Microsoft.Cpp\v4.0\V120\
 set VCTargetsPath12=%BuildLabSetupFilesRoot%\MSBuild\Microsoft.Cpp\v4.0\V120\
 set WindowsSDK_LibraryPath_x64=%BuildLabSetupFilesRoot%\Windows Kits\10\Lib\10.0.14393.0\um\x64
 set WindowsSDK_LibraryPath_x86=%BuildLabSetupFilesRoot%\Windows Kits\10\Lib\10.0.14393.0\um\x86
-set IncludePath=%BuildLabSetupFilesRoot%\Windows Kits\10\Include\10.0.14393.0\um;%BuildLabSetupFilesRoot%\Windows Kits\10\Include\10.0.14393.0\shared;%BuildLabSetupFilesRoot%\Microsoft Visual Studio 12.0\vc\include
+set WindowsSDK_IncludePath=%BuildLabSetupFilesRoot%\Windows Kits\10\Include\10.0.14393.0\um;%BuildLabSetupFilesRoot%\Windows Kits\10\Include\10.0.14393.0\shared

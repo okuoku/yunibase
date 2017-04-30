bd182d42df017ab963685348694cdc1931f79864
okuoku <mjt@cltn.org>
2017-04-30 16:33:49 +0900

    Gambit: Use `from-scratch` target to bootstrap

--- recipes/gambit.recipe.cmake
+++ recipes/gambit.recipe.cmake
@@ -11,7 +11,5 @@ set(RECIPE_GAMBIT_BOOTSTRAP
     STEP "SetupGscBoot" cp ${YUNIBASE_BUILD_STABLE_PREFIX}/gambit/bin/gsc
     gsc-boot
     STEP "ConfigureFirst" "./configure" "--enable-single-host"
-    STEP "Bootstrap1" MAKE bootclean
-    STEP "Bootstrap2" MAKE bootstrap
-    STEP "Bootstrap3" MAKE bootclean
+    STEP "Bootstrap" MAKE __MAKE_PARALLEL__ __MAKE_OPTS__ from-scratch
     ${RECIPE_GAMBIT})

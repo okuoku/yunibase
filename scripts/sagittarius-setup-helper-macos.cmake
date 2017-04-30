926a817efc743e4bc81bb6a2b306bb7bae7bf96a
okuoku <mjt@cltn.org>
2017-04-30 22:19:22 +0900

    Sagittarius: Fix macOS build
    Feed dylib name with version to install_name_tool.

--- scripts/sagittarius-setup-helper-macos.cmake
+++ scripts/sagittarius-setup-helper-macos.cmake
@@ -11,11 +11,13 @@ set(EXEC ${PTH}/bin/sagittarius)
 
 file(GLOB_RECURSE dylibs "${PTH}/*.dylib")
 
+set(libsagittarius_name)
+
 function(apply_install_name_tool fil)
     execute_process(
         COMMAND
-        install_name_tool -change libsagittarius.dylib
-        @executable_path/../lib/libsagittarius.dylib
+        install_name_tool -change ${libsagittarius_name}
+        @executable_path/../lib/${libsagittarius_name}
         ${fil}
         RESULT_VARIABLE rr
         )
@@ -25,6 +27,22 @@ function(apply_install_name_tool fil)
         message(STATUS "Modified: ${fil}")
     endif()
 endfunction()
+
+foreach(e ${dylibs})
+    get_filename_component(base ${e} NAME)
+    if(${base} MATCHES "libsagittarius\\.[0-9]+\\.[0-9]+\\.[0-9]+\\.dylib")
+        if(libsagittarius_name)
+            message(STATUS
+                "Multiple libsagittarius found! before: ${libsagittarius_name_full} after: ${e}")
+        endif()
+        set(libsagittarius_name_full ${e})
+        set(libsagittarius_name ${CMAKE_MATCH_0})
+    endif()
+endforeach()
+
+if(NOT libsagittarius_name)
+    message(FATAL_ERROR "libsagittarius not found in ${dylibs}")
+endif()
 
 foreach(e ${EXEC} ${dylibs})
     apply_install_name_tool(${e})

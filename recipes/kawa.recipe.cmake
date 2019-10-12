f8934f3881d3edf7433fb37c8dbb210b974b75ee
okuoku <mjt@cltn.org>
2019-10-13 02:09:28 +0900

    Kawa: Use automake to build Kawa

--- recipes/kawa.recipe.cmake
+++ recipes/kawa.recipe.cmake
@@ -2,7 +2,8 @@
 # There is no bootstrap/test phase for kawa.
 
 set(RECIPE_KAWA
-    STEP "Build" ant -Ddist.kawa.jar=kawa.jar
-    STEP "Postbuild" mkdir -p __INSTALL_PREFIX__
-    STEP "Install" cp kawa.jar __INSTALL_PREFIX__
+    STEP "Automake" ./autogen.sh
+    STEP "Configure" ./configure "--prefix=__INSTALL_PREFIX__"
+    STEP "Build" make __MAKE_PARALLEL__ __MAKE_OPTS__
+    STEP "Install" make install
 )

71911e9a77a389e73cf76397b635ae3c1ff13a65
okuoku <mjt@cltn.org>
2019-10-13 03:41:45 +0900

    Kawa: Remove parallel option

--- recipes/kawa.recipe.cmake
+++ recipes/kawa.recipe.cmake
@@ -4,6 +4,6 @@
 set(RECIPE_KAWA
     STEP "Automake" ./autogen.sh
     STEP "Configure" ./configure "--prefix=__INSTALL_PREFIX__"
-    STEP "Build" make __MAKE_PARALLEL__ __MAKE_OPTS__
+    STEP "Build" make __MAKE_OPTS__
     STEP "Install" make install
 )

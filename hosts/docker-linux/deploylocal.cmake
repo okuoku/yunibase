73187e9479f5804a7871e7fc4a73501a6d0fc5ba
okuoku <mjt@cltn.org>
2020-03-28 23:17:46 +0900

    Docker linux: Fix SCM installation

--- hosts/docker-linux/deploylocal.cmake
+++ hosts/docker-linux/deploylocal.cmake
@@ -4,6 +4,17 @@
 # INPUTs:
 #   ARCHIVE: Full path to archive
 #   IMPLNAM: Implementation dir name
+#   BRANCH: current/stable
+
+if(NOT BRANCH)
+    set(BRANCH current)
+elseif(${BRANCH} STREQUAL CURRENT)
+    set(BRANCH current)
+elseif(${BRANCH} STREQUAL STABLE)
+    set(BRANCH stable)
+else()
+    message(FATAL_ERROR "Unknown branch name: ${BRANCH}")
+endif()
 
 if(NOT IMPLNAM)
     message(FATAL_ERROR "Huh?")
@@ -16,15 +27,15 @@ if(NOT EXISTS ${ARCHIVE})
     message(STATUS "File ${ARCHIVE} not found (Skip)")
     return()
 endif()
-set(destdir /opt/yunibase/current/${IMPLNAM})
+set(destdir /opt/yunibase/${BRANCH}/${IMPLNAM})
 
 if(EXISTS ${destdir})
     file(REMOVE_RECURSE ${destdir})
 endif()
 
-file(MAKE_DIRECTORY /opt/yunibase/current)
+file(MAKE_DIRECTORY /opt/yunibase/${BRANCH})
 
 execute_process(
     COMMAND tar zxf ${ARCHIVE}
-    WORKING_DIRECTORY /opt/yunibase/current)
+    WORKING_DIRECTORY /opt/yunibase/${BRANCH})
 

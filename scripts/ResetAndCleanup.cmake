376f702b4e98e4db282aeb0014a2dd30e973dd79
okuoku <mjt@cltn.org>
2020-06-07 05:53:13 +0900

    scripts: ResetAndCleanup

---
+++ scripts/ResetAndCleanup.cmake
@@ -0,0 +1,20 @@
+message(STATUS "Reset...")
+execute_process(
+    COMMAND git submodule foreach --recursive git reset --hard
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
+    )
+message(STATUS "Cleanup...")
+execute_process(
+    COMMAND git submodule foreach --recursive git clean -xfd .
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
+    )
+message(STATUS "Sync URLs...")
+execute_process(
+    COMMAND git submodule sync --recursive
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
+    )
+message(STATUS "Checkout...")
+execute_process(
+    COMMAND git submodule update
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/..
+    )

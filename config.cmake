aca971642c7bc001b9ae89cc2c5d88a11bc091cf
okuoku <mjt@cltn.org>
2020-06-07 07:57:52 +0900

    single: New build script for non-Linux platforms

---
+++ config.cmake
@@ -0,0 +1,15 @@
+set(cygwin64-impls
+    #GUILE3
+    #GAUCHE
+    CHIBI_SCHEME
+    #CHICKEN5
+    S7YUNI
+
+    # SAGITTARIUS
+    # CYCLONE
+    # FOMENT
+    # GAMBIT
+    #
+    # STKLOS
+    # BIGLOO
+    )
---
+++ scripts/buildstep.cmake
@@ -0,0 +1,43 @@
+if(NOT DIR) # Impl builddir 
+    message(FATAL_ERROR "huh?")
+endif()
+
+if(NOT NAME) # Impl CMake name "chibi-scheme"
+    message(FATAL_ERROR "huh?")
+endif()
+
+if(NOT ARCHIVE) # CMake path for archive
+    message(FATAL_ERROR "huh?")
+endif()
+
+# Remove existing impl.
+message(STATUS "Cleanup...${NAME}")
+execute_process(
+    COMMAND ${CMAKE_COMMAND} -E remove_directory
+    /opt/yunibase/stable/${NAME})
+execute_process(
+    COMMAND ${CMAKE_COMMAND} -E remove_directory
+    /opt/yunibase/current/${NAME})
+
+# Build
+message(STATUS "Build...${NAME}")
+execute_process(
+    COMMAND ${CMAKE_COMMAND} --build ${DIR}
+    RESULT_VARIABLE rr)
+
+if(rr)
+    message(FATAL_ERROR "Error build ${NAME}")
+endif()
+
+# Generate archive
+message(STATUS "Archive...${NAME}")
+execute_process(
+    COMMAND ${CMAKE_COMMAND} -E tar cvzf ${ARCHIVE}
+    ${NAME}
+    WORKING_DIRECTORY /opt/yunibase/current
+    RESULT_VARIABLE rr)
+
+if(rr)
+    message(FATAL_ERROR "Error archive ${NAME}")
+endif()
+

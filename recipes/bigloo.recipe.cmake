d86e1106ae985fe53696a0967a511a4647bc4f12
okuoku <mjt@cltn.org>
2020-06-07 19:00:36 +0900

    Bigloo: Use cygpath

--- recipes/bigloo.recipe.cmake
+++ recipes/bigloo.recipe.cmake
@@ -1,6 +1,18 @@
+if(CYGWIN)
+    execute_process(
+        COMMAND cygpath -m -d /
+        OUTPUT_VARIABLE BIGLOO_CYGWINDOSPATH_OUT
+        )
+    set(BIGLOO_CYGWINDOSPATH
+        --cygwin-dos-path=${BIGLOO_CYGWINDOSPATH_OUT}
+        --cygwin-dos-jvm=no)
+else()
+    set(BIGLOO_CYGWINDOSPATH)
+endif()
+
 set(RECIPE_BIGLOO_STABLE
     STEP "Configure" 
-    ./configure --prefix=__INSTALL_PREFIX__
+    ./configure --prefix=__INSTALL_PREFIX__ ${BIGLOO_CYGWINDOSPATH}
     STEP "Build" 
     MAKE __MAKE_PARALLEL__
     STEP "Install"
@@ -10,7 +22,7 @@ set(RECIPE_BIGLOO_STABLE
 
 set(RECIPE_BIGLOO_CURRENT
     STEP "Configure"
-    ./configure --prefix=__INSTALL_PREFIX__
+    ./configure --prefix=__INSTALL_PREFIX__ ${BIGLOO_CYGWINDOSPATH}
     STEP "Build"
     MAKE bigboot BGLBUILDBINDIR=${YUNIBASE_BUILD_STABLE_PREFIX}/bigloo/bin
     STEP "InstallProgs"

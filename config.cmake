e2583834e583742b6f2324325ba6b0e8ce7d0dd2
okuoku <mjt@cltn.org>
2020-06-07 21:03:55 +0900

    cygwin64: Additional comments

--- config.cmake
+++ config.cmake
@@ -18,11 +18,10 @@ set(cygwin64-impls
     CHICKEN5
     S7YUNI
     SAGITTARIUS
-    GAMBIT
 
-    # FOMENT
-    #
     # CYCLONE # Cygwin does not have Concurrency Kit package
     # STKLOS # Cygwin support broken on Win64 (Cygwin 1.7 or later)
     # BIGLOO # Current build fail
+    # GAMBIT # RENAME_NOREPLACE misplaced in Cygwin
+    # FOMENT # Depends on <execinfo.h> backtrace(3)
     )

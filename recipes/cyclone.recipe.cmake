a44a56982aa5510f031f2e06bc66a1ec8077dbf3
okuoku <mjt@cltn.org>
2020-06-09 19:54:59 +0900

    Cyclone: Add CFLAGS/LDFLAGS to support compilation on macOS

--- recipes/cyclone.recipe.cmake
+++ recipes/cyclone.recipe.cmake
@@ -1,6 +1,6 @@
 set(RECIPE_CYCLONE
     STEP "Build" 
-    MAKE "PREFIX=__INSTALL_PREFIX__"
+    MAKE "PREFIX=__INSTALL_PREFIX__" __AUTOCONF_OPTS__
     STEP "Install"
     MAKE "PREFIX=__INSTALL_PREFIX__" install
     STEP "Test"

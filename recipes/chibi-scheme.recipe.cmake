5b7255ec26df162f7fcf564ff9572eb079568916
okuoku <mjt@cltn.org>
2022-10-11 17:58:35 +0900

    Chibi-scheme: Handle `ldconfig` issue on DragonFly BSD
    On DragonFly BSD, running `ldconfig` might cause infinite-loop
    to create temporary file under `/var/run`.
    
    open("/var/run/ld-elf.so.hints.uaB7nO",0xa02,0600) ERR#17 'File exists'
    open("/var/run/ld-elf.so.hints.vaB7nO",0xa02,0600) ERR#17 'File exists'
    open("/var/run/ld-elf.so.hints.waB7nO",0xa02,0600) ERR#17 'File exists'
    
    specify some invalid value on variable `LDCONFIG` to prevent `ldconfig`
    running.

--- recipes/chibi-scheme.recipe.cmake
+++ recipes/chibi-scheme.recipe.cmake
@@ -8,8 +8,14 @@
 #   test-dist
 # 
 # We use test-all here for now.
+if(CMAKE_SYSTEM_NAME STREQUAL DragonFly)
+    # Prevent illegal ldconfig run that can cause infinite-loop on non-root
+    set(ldconfig "LDCONFIG=NEVERLAND")
+else()
+    set(ldconfig)
+endif()
 set(RECIPE_CHIBI_SCHEME 
-    STEP "Build" MAKE "PREFIX=__INSTALL_PREFIX__"
+    STEP "Build" MAKE ${ldconfig} "PREFIX=__INSTALL_PREFIX__"
     STEP "Test" MAKE test-all "PREFIX=__INSTALL_PREFIX__"
-    STEP "Install" MAKE install "PREFIX=__INSTALL_PREFIX__"
+    STEP "Install" MAKE install ${ldconfig} "PREFIX=__INSTALL_PREFIX__"
 )
